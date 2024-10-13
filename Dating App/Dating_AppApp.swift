232d3e2714605f4a60b3da29015f60e43aa2fdc2
Mitchel Carson <carsonma@appstate.edu>
2024-10-13 13:14:33 -0400

    Initial Commit

---
+++ Dating App/Dating_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dating_AppApp.swift
+//  Dating App
+//
+//  Created by Mitchel Carson on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Dating_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dating AppTests/Dating_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dating_AppTests.swift
+//  Dating AppTests
+//
+//  Created by Mitchel Carson on 10/13/24.
+//
+
+import Testing
+@testable import Dating_App
+
+struct Dating_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
