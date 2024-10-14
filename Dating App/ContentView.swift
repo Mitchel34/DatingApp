e7b2df0e4df8398e94b4965656fc7e3ae5de21ed
Mitchel Carson <carsonma@appstate.edu>
2024-10-14 19:59:52 -0400

    Welcome page content updates

--- Dating App/ContentView.swift
+++ Dating App/ContentView.swift
@@ -10,10 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "heart")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Image(systemName: "heart")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Welcome!")
+            Text("Dating App is a place for Appalachian State University students to meet new people and find connection.")
         }
         .padding()
     }
